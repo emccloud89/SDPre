149b967e1aeb8ab1286fe00bb8ac984e5748f205
Elliot <elliotmccloud2@icloud.com>
2025-05-11 16:49:35 -0600

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Elliot Mccloud";
+    int age = 36;
+    String state = "North Dakota";
+
+    /*
+      System.out.println(name age state);
+    */
+    
+  }
+}

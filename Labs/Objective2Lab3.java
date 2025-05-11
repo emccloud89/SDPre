972c847c2e922e463cc1573fcf42304454f357a5
Elliot <elliotmccloud2@icloud.com>
2025-05-11 16:50:25 -0600

    Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+      int first_number = 1989;
+      String month = "february";
+      boolean isSaturday = true;
+      boolean isSunday = true;
+    /*
+      System.out.println(int);
+      System.out.println(month);
+      System.out.println(isSunday);
+    */
+
+  }
+}

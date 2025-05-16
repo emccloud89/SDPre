1f5d0c492f77e483e7dc9929df522541ca3a8208
Elliot <elliotmccloud2@icloud.com>
2025-05-15 21:19:11 -0600

    Objective 8

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,16 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i;
+    i = 1;
+    for (i = 1; i <=10; i++) {
+      System.out.println(i);
+    }
+
+    for (i = 10; i >= 1; i--) {
+      System.out.println(i);
+    }
+    //TODO Write a for loop that will print the numbers 1 to 10
+
+    //TODO Write another for loop that will print the numbers 10 to 1.
+  }
+}
